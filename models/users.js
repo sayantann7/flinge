bf5c13c5cdd7f624d0b350af89f395fccdffa96c
Sayantan Nandi <officialsayantannandi@gmail.com>
2024-11-25 14:32:35 +0530

    Ready for deployment

--- models/users.js
+++ models/users.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/flinge", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
